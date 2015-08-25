38c2f7060c0e973db5194dddb681b281ecb64f6b
Josh Morin <JoshMorin@gmx.com>
2015-08-25 09:43:32 +0300

    rust docs

--- documentation/implementors/core/iter/trait.Iterator.js
+++ documentation/implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['shams'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='shams/struct.YearIter.html' title='shams::YearIter'>YearIter</a>",];
+implementors['shams'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='shams/struct.ShamsYear.html' title='shams::ShamsYear'>ShamsYear</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ update_docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+cd documentation/__documentation
+cargo update
+cargo clean
+cargo doc
+cd -
+cp -r documentation/__documentation/target/doc/* documentation
+
+
